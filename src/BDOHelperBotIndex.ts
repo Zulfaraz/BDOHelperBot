8f36a20e159dd8eef3eeaa4aa55201eaf4e7440e
Daniel Leonardo Arevalo Padilla <kiminordther@gmail.com>
2021-04-19 23:31:11 -0500

    19_04_2021_11_30_PM
    Se modifico algunos archivos para que no se subiera cierta documentacion.

--- src/BDOHelperBotIndex.ts
+++ src/BDOHelperBotIndex.ts
@@ -1,2 +1,11 @@
 import Discord,{ Client } from 'discord.js'
+import { config } from 'dotenv'
+config()
 
+const BDOHelperBot: Client = new Client()
+
+BDOHelperBot.on('ready', async() =>{
+    console.log("The servicies of the BDOHelperBot is on!")
+})
+
+BDOHelperBot.login(process.env.BDOHElPERBOT_TOKEN)
